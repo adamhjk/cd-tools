672074a608b912eb7f80504b2cac5564e3026272
Adam Jacob <adam@opscode.com>
2012-07-31 12:35:39 -0700

    using an http proxy for jenkins

--- chef-repo/roles/jenkins.rb
+++ chef-repo/roles/jenkins.rb
@@ -1,3 +1,10 @@
 name "jenkins"
 description "Jenkins Server"
 run_list [ "recipe[yum]", "recipe[build-essential]", "recipe[java]", "recipe[jenkins]", "recipe[postgresql::server]", "recipe[gerrit]" ]
+default_attributes(
+  'jenkins' => {
+    'http_proxy' => {
+      'variant' => 'nginx'
+    }
+  }
+)

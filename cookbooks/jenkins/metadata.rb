03351038a0c06dc08d22c6b288031ae0f2f74d25
Jenkins <jenkins@jenkins.local>
2012-08-02 04:11:56 +0000

    Updated patch level for gerrit, jenkins, mysql, runit, ohai, windows, java, nginx, openssl, yum, aws, database, build-essential, chef_handler, postgresql, bluepill, foodcritic, cd-tools

--- cookbooks/jenkins/metadata.rb
+++ cookbooks/jenkins/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "aj@junglist.gen.nz"
 license          "Apache 2.0"
 description      "Installs and configures Jenkins CI server & slaves"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.5"
+version          "0.6.6"
 
 depends "apache2"
 depends "nginx"

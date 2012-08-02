4eb6ea82855d848b7ee4fa3a12d0dba556833cd1
Jenkins <jenkins@jenkins.local>
2012-08-02 04:09:38 +0000

    Updated patch level for gerrit, jenkins, mysql, runit, ohai, windows, java, nginx, openssl, yum, aws, database, build-essential, chef_handler, postgresql, bluepill, foodcritic, cd-tools

--- cookbooks/foodcritic/metadata.rb
+++ cookbooks/foodcritic/metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures foodcritic"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 depends "build-essential"

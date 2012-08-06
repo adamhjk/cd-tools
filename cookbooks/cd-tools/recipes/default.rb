81de7249e69c1c4dc1d403951583b13b7baa5ceb
Adam Jacob <adam@opscode.com>
2012-08-06 11:09:42 -0700

    Auto-populate the tools
    Change-Id: Ia7a1e20fe4aaf4c3aff55a31a4dfcab49ee861f1

--- cookbooks/cd-tools/recipes/default.rb
+++ cookbooks/cd-tools/recipes/default.rb
@@ -6,3 +6,15 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+# create up to 10 backups of the files, set the files owner different from the directory.
+remote_directory "/var/lib/jenkins/tools" do
+  source "tools"
+  files_backup 10
+  files_owner "root"
+  files_mode "0755"
+  owner "root"
+  mode "0755"
+end
+
